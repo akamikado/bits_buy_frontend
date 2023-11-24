3782d1003d188f21cadb57f71ee69d16efac771c
Srichandra Lolla <129283037+srichandra707@users.noreply.github.com>
2023-11-24 11:45:25 +0530

    Revert "UserInput functionality to send entered user data"
    This reverts commit 320713886a99d1ba7e2eff614d22176df3090d6e.

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -3,7 +3,8 @@ import { TextInput, PasswordInput, Tooltip, Center, Text, rem, Button } from '@m
 import { IconInfoCircle } from '@tabler/icons-react';
 import classes from './UserForm.module.css';
 
-  const ToolTipIcon = (
+function TooltipIcon() {
+  const rightSection = (
     <Tooltip
       label="We store your data securely"
       position="top-end"
@@ -17,29 +18,24 @@ import classes from './UserForm.module.css';
       </Text>
     </Tooltip>
   );
-function EmailInput(){
 
-  return(
+  return (
     <TextInput
-      rightSection={ToolTipIcon}
-      label="Email"
-      type="email"
-      required
+      rightSection={rightSection}
+      label="Name"
       placeholder=""
       className={classes['text-input']}
     />
   );
-  
 }
 
-
-function PassInput() {
+function TooltipFocus() {
   const [opened, setOpened] = useState(false);
   const [value, setValue] = useState('');
-  const valid = value.trim().length === 13;
+  const valid = value.trim().length >= 6;
   return (
     <Tooltip
-      label={valid ? 'All good!' : 'Phone Number should have +91 and 10 digits'}
+      label={valid ? 'All good!' : 'Phone Number should have +91'}
       position="bottom-start"
       withArrow
       opened={opened}
@@ -62,47 +58,11 @@ function PassInput() {
 }
 
 export function UserForm() {
-  const [userEmail, setUserEmail] = useState('');
-  const [userPhone, setUserPhone] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
-
-  const FormSubmissionHandler = async (event) => {
-    event.preventDefault(); // Prevent default form submission
-
-    try {
-      const response = await fetch('xyz.com', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          email: userEmail,
-          phone: userPhone,
-        }),
-      });
-
-      const responseData = await response.json();
-
-      if (!response.ok) {
-        throw new Error(`Error: ${responseData.message}`);
-      }
-
-      // Handle successful response
-      console.log('User data successfully submitted:', responseData);
-    } catch (error) {
-      setErrorMessage(`Error: ${error.message}`);
-      console.error('Error submitting user data:', error);
-    }
-  };
-
   return (
     <>
-      <EmailInput value={userEmail} onChange={(event) => setUserEmail(event.currentTarget.value)} />
-      <PassInput value={userPhone} onChange={(event) => setUserPhone(event.currentTarget.value)} />
-      {errorMessage && <p className={classes['error-message']}>{errorMessage}</p>}
-      <Button variant="light" color="teal" size="lg" radius="xl" onClick={FormSubmissionHandler}>
-        Submit
-      </Button>
+      <TooltipIcon />
+      <TooltipFocus />
+      <Button variant="light" color="teal" size="lg" radius="xl" >Submit</Button>
     </>
   );
 }
