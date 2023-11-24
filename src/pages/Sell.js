5f1922237ec6d8648720d2c15224c3736a986427
Srichandra Lolla <129283037+srichandra707@users.noreply.github.com>
2023-11-24 12:40:09 +0530

    added navbars to Sell and UserProfile

--- src/pages/Sell.js
+++ src/pages/Sell.js
@@ -1,16 +1,38 @@
 import { FeatureCard } from "../components/Card";
-function Sell(){
-    return(
+import { useState } from "react";
+import Navbar from "../components/navbar";
+import classes from "./Sell.module.css";
+function Sell() {
+  const [isNavbarOpen, setIsNavbarOpen] = useState(false);
+  const toggleNavbar = () => {
+    setIsNavbarOpen((prevIsNavbarOpen) => !prevIsNavbarOpen);
+  };
+  const closeNavbar = () => {
+    setIsNavbarOpen(false);
+  };
+  return (
+    <div className={classes["home-page-container"]}>
+      {!isNavbarOpen && (
+        <button onClick={toggleNavbar} className={classes["nav-button"]}>
+          &#9776;
+        </button>
+      )}
+      {isNavbarOpen && (
+        <div className={classes["overlay"]} onClick={closeNavbar}></div>
+      )}
+      {/* Navbar */}
+      {isNavbarOpen && <Navbar isOpen={isNavbarOpen} />}
+      {!isNavbarOpen && (
         <div>
-        <h1>Sell</h1>
-        <div>
-        <FeatureCard />
-        <FeatureCard />
-        
-
+          <h1>Sell</h1>
+          <div>
+            <FeatureCard />
+            <FeatureCard />
+          </div>
         </div>
-        </div>
-    );
+      )}
+    </div>
+  );
 }
 
 export default Sell;
