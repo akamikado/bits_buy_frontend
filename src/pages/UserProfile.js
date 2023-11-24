f6f7a9e19bb17b99fd603e4b5d580af0f7303711
Srichandra Lolla <129283037+srichandra707@users.noreply.github.com>
2023-11-24 12:09:58 +0530

    User_Input taken and sent to backend

--- src/pages/UserProfile.js
+++ src/pages/UserProfile.js
@@ -1,12 +1,13 @@
-
 import UserForm from "../components/UserForm";
 import classes from "./UserProfile.module.css";
 function UserProfile() {
   return (
-    <div className={classes['user-profile-container']}>
-      <UserForm className={classes['user-form']}/>
-      <h1>User Profile</h1>
-    </div>
+      <div className={classes["user-profile-container"]}>
+        <h1>User Profile</h1>
+        <div className={classes["Input-box"]}>
+          <UserForm className={classes["user-form"]} />
+        </div>
+      </div>
   );
 }
 
